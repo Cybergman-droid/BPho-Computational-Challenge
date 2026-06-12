4ff89175c636c6592076db99aa1d4b163d368607
Cybergman-droid <jasongakpo20@gmail.com>
2026-06-12 13:15:30 +0100

    Remove unused random walk functions and related console logs

--- testing.js
+++ testing.js
@@ -137,41 +137,6 @@ const config = {
 };
 const chart = new Chart(ctx, config);
 
-function randomWalk(N, stepSize) {
-	let points = [{ x: 0, y: 0 }];
-	for (let i = 1; i <= N; i++) {
-		let angleRad = Math.random() * (2 * Math.PI);
-		let xcoordRad = Math.cos(angleRad) * stepSize;
-		let ycoordRad = Math.sin(angleRad) * stepSize;
-		console.log(angleRad);
-		console.log(xcoordRad);
-		console.log(ycoordRad);
-		let x = points[i - 1].x + xcoordRad;
-		let y = points[i - 1].y + ycoordRad;
-
-		points.push({ x, y });
-	}
-	console.log(points);
-	return points;
-}
-
-function randomWalkGenerator(numOfWalks) {
-	let datasets = [];
-	for (let i = 0; i <= numOfWalks; i++) {
-		let walk = randomWalk(50, 1);
-		let dataSet = {
-			label: `walk${i}`,
-			data: walk,
-			borderColor: colours[i % colours.length],
-			borderWidth: 0.5,
-			showLine: true,
-			pointRadius: 0,
-		};
-		datasets.push(dataSet);
-	}
-	return datasets;
-}
-
 const reloadBtn = document.getElementById("reloadBtn");
 reloadBtn.addEventListener("click", () => {
 	dataSets = randomWalkGenerator(50);
